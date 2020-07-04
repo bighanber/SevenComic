2d28b004c1e46a3c1f26a66483e3065425ed031b
uuluu <358906125@qq.com>
2020-07-04 12:20:17 +0800

    首页推荐

--- seven_comic/lib/ui/main/home_recommend_page.dart
+++ seven_comic/lib/ui/main/home_recommend_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:provider/provider.dart';
+import 'package:sevencomic/entity/index_entity.dart';
 import 'package:sevencomic/provider/provider_widget.dart';
+import 'package:sevencomic/ui/main/recommend_list_item.dart';
 import 'package:sevencomic/view_model/recommend_model.dart';
 import 'package:sevencomic/widget/banner_image.dart';
 
@@ -13,16 +15,13 @@ class HomeRecommendPage extends StatefulWidget {
   }
 }
 
-class _HomeRecommendPage extends State<HomeRecommendPage> with AutomaticKeepAliveClientMixin {
-
+class _HomeRecommendPage extends State<HomeRecommendPage>
+    with AutomaticKeepAliveClientMixin {
   var bannerHeight;
 
   @override
   Widget build(BuildContext context) {
-    bannerHeight = MediaQuery
-        .of(context)
-        .size
-        .width * 4 / 7;
+    bannerHeight = MediaQuery.of(context).size.width * 4 / 6;
 
     return ProviderWidget<RecommendModel>(
       onModelReady: (recommendModel) {
@@ -30,9 +29,34 @@ class _HomeRecommendPage extends State<HomeRecommendPage> with AutomaticKeepAliv
       },
       builder: (context, recommendModel, child) {
         return Scaffold(
-          body: Container(
-            height: bannerHeight,
-            child: BannerWidget(bannerHeight),
+          body: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(),
+              SliverAppBar(
+                brightness: Theme.of(context).brightness == Brightness.light &&
+                        recommendModel.isBusy
+                    ? Brightness.light
+                    : Brightness.dark,
+                actions: [],
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Container(
+                    height: bannerHeight,
+                    child: BannerWidget(bannerHeight),
+                  ),
+                  centerTitle: true,
+                ),
+                expandedHeight: bannerHeight,
+                pinned: true,
+              ),
+              if (recommendModel.isEmpty)
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 50),
+                    child: Text("empty"),
+                  ),
+                ),
+              HomeRecommendList(),
+            ],
           ),
         );
       },
@@ -45,7 +69,6 @@ class _HomeRecommendPage extends State<HomeRecommendPage> with AutomaticKeepAliv
 }
 
 class BannerWidget extends StatelessWidget {
-
   var bannerHeight;
 
   BannerWidget(this.bannerHeight);
@@ -54,34 +77,59 @@ class BannerWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: Theme
-            .of(context)
-            .scaffoldBackgroundColor,
+        color: Theme.of(context).scaffoldBackgroundColor,
       ),
-      child: Consumer<RecommendModel>(
-          builder: (context, recommendModel, child) {
-            if (recommendModel.isBusy) {
-              return CupertinoActivityIndicator();
-            } else {
-              var banners = recommendModel?.banners ?? [];
-              return Swiper(
-                loop: true,
-                autoplay: true,
-                autoplayDelay: 5000,
-                pagination: SwiperPagination(),
-                itemCount: banners.length,
-                itemBuilder: (context, index) {
-                  return InkWell(
-                    onTap: () {
-                      var banner = banners[index];
-                    },
-                    child: BannerImage(banners[index].cover, bannerHeight, fit: BoxFit.fill,),
-                  );
+      child:
+          Consumer<RecommendModel>(builder: (context, recommendModel, child) {
+        if (recommendModel.isBusy) {
+          return CupertinoActivityIndicator();
+        } else {
+          var banners = recommendModel?.banners ?? [];
+          return Swiper(
+            loop: true,
+            autoplay: true,
+            autoplayDelay: 5000,
+            pagination: SwiperPagination(),
+            itemCount: banners.length,
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+                  var banner = banners[index];
                 },
+                child: BannerImage(
+                  banners[index].cover,
+                  bannerHeight,
+                  fit: BoxFit.fill,
+                ),
               );
-            }
-          }),
+            },
+          );
+        }
+      }),
     );
   }
+}
 
+class HomeRecommendList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    RecommendModel model = Provider.of(context);
+    if (model.isBusy) {
+      return SliverToBoxAdapter(
+        child: Text("loading"),
+      );
+    }
+
+    return SliverList(
+        delegate: SliverChildBuilderDelegate(
+      (context, index) {
+        IndexEntity item = model.content[index];
+        return RecommendItemWidget(
+          item,
+          index: index,
+        );
+      },
+      childCount: model.content?.length ?? 0,
+    ));
+  }
 }
---
+++ seven_comic/lib/ui/main/recommend_list_item.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sevencomic/entity/index_entity.dart';
+
+class RecommendItemWidget extends StatelessWidget {
+  IndexEntity data;
+  int index;
+
+  RecommendItemWidget(this.data, {this.index})
+      : super(key: ValueKey(data.categoryId));
+
+  @override
+  Widget build(BuildContext context) {
+    var backgroundColor = Theme.of(context).scaffoldBackgroundColor;
+
+    UniqueKey key = UniqueKey();
+
+    return Material(
+      color: backgroundColor,
+      child: Card(
+        elevation: 0,
+        margin: EdgeInsets.only(top: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(15, 10, 0, 10),
+              child: Text(
+                data.title,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Color(0xff333333),
+                    fontWeight: FontWeight.w500),
+              ),
+            ),
+            GridView.builder(
+                shrinkWrap: true,
+                itemCount: data.data.length,
+                physics: NeverScrollableScrollPhysics(),
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: (data.sort == 5 || data.sort == 9 || data.sort == 11) ? 2 : 3,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  childAspectRatio:
+                      (data.sort == 5 || data.sort == 9 || data.sort == 11) ? 1.8 : 0.56,
+                ),
+                itemBuilder: (context, index) {
+                  IndexData item = data.data[index];
+                  return RecommendSubItemWidget(
+                    item,
+                    index: index,
+                    sort: data.sort,
+                  );
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RecommendSubItemWidget extends StatelessWidget {
+  IndexData data;
+  int index;
+  double sort;
+
+  RecommendSubItemWidget(this.data, {this.index, this.sort})
+      : super(key: ValueKey(data.objId));
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        child: (sort != 5 && sort != 9 && sort != 11)
+            ? Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image(
+                    fit: BoxFit.fill,
+                    image: NetworkImage(data.cover,
+                        headers: {"Referer": "http://v3api.dmzj.com/"}),
+                  ),
+                  Text(data.title, style: TextStyle(fontSize: 14, color: Color(0xff333333),),maxLines: 1,overflow: TextOverflow.ellipsis,),
+                  Text(data.status, style: TextStyle(fontSize: 12, color: Color(0xff666666)),),
+                ],
+              )
+            : Image(
+                fit: BoxFit.fill,
+                image: NetworkImage(data.cover,
+                    headers: {"Referer": "http://v3api.dmzj.com/"}),
+              ),
+      ),
+    );
+  }
+}
